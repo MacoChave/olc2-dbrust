86d0d5ac0ee2c8a4a5a93d34f9ffffa651ae5307
MacoChave <201020831@ingenieria.usac.edu.gt>
2022-03-04 00:27:42 -0600

    Editor mejorado

--- App.go
+++ App.go
@@ -1,34 +1,72 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
+var templates = template.Must(template.ParseGlob("template/*"))
+
+type Response struct {
+	Source string
+	Result string
+}
+
 func main() {
-	temp := template.Must(template.ParseFiles("template/index.html"))
+	http.HandleFunc("/", index)
+	http.HandleFunc("/analize", analizar)
+	// http.HandleFunc("/report", reports)
+	http.HandleFunc("/about", about)
 
-	http.HandleFunc("/analizar", func(rw http.ResponseWriter, r *http.Request) {
-		sourceCode := r.FormValue("source")
-		_ = sourceCode
+	// Original <=============Funciona
+	// temp := template.Must(template.ParseFiles("template/index.html"))
 
-		// TODO: ANALIZAR ENTRADA
-		resultado := "Resultado"
+	// http.HandleFunc("/analizar", func(rw http.ResponseWriter, r *http.Request) {
+	// 	sourceCode := r.FormValue("source")
+	// 	_ = sourceCode
 
-		temp.Execute(rw, struct {
-			Source string
-			Result string
-		}{
-			Source: sourceCode,
-			Result: resultado,
-		})
-	})
+	// 	// TODO: ANALIZAR ENTRADA
+	// 	resultado := "Resultado"
 
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		err := temp.Execute(rw, nil)
-		fmt.Println(err)
-	})
+	// 	temp.Execute(rw, struct {
+	// 		Source string
+	// 		Result string
+	// 	}{
+	// 		Source: sourceCode,
+	// 		Result: resultado,
+	// 	})
+	// })
 
+	// http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+	// 	err := temp.Execute(rw, nil)
+	// 	fmt.Println(err)
+	// })
+
+	log.Println("http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func index(rw http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(rw, "index", nil)
+}
+
+func analizar(rw http.ResponseWriter, r *http.Request) {
+	sourceCode := r.FormValue("source")
+	// TODO: Analize source code
+	result := "Result here"
+
+	res := Response{
+		Source: sourceCode,
+		Result: result,
+	}
+	templates.ExecuteTemplate(rw, "index", res)
+}
+
+// func reports(rw http.ResponseWriter, r *http.Request) {
+// 	templates.ExecuteTemplate(rw, "index")
+// }
+
+func about(rw http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(rw, "about", nil)
 }
